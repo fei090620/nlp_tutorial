6d0e8dba7f1669c2af5dd43405178e22af8aa26c
yangzhou <yzhou3_05@sina.com>
2018-04-17 11:02:49 +0800

    [zhouyang] #n/a implementation km and hierarchical cluster

--- src/Hierarchical_clustering.py
+++ src/Hierarchical_clustering.py
@@ -1,5 +1,64 @@
+# ! -*- coding:utf-8 -*-
+import logging
+
+import numpy as np
+import os
+from gensim.models import doc2vec
+from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
 
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-class Hierarchical_clustering(object):
-    pass
+file_path = lambda: os.path.dirname(__file__)
+
+doc2vec_model_file = file_path() + '/../data/word2vecs/doc2vec_model.txt'
+
+map_file = file_path() + '/../data/word2vecs/tagged_map.txt'
+
+def train_cluster_by_hierarchical(vectors, num_clusters=10):
+    # scipy的层次聚类（可以后去聚类的过程）
+    clusters_result = linkage(vectors, method='ward', metric='euclidean')
+
+    #sklearn 层次聚类
+    # hierachical = AgglomerativeClustering(n_clusters=num_clusters,
+    #                                       affinity="euclidean",
+    #                                       compute_full_tree=True,
+    #                                       linkage='ward')
+
+    return clusters_result
+
+def show_cluster_progresses(cluster_result):
+    plt.figure(figsize=(10, 8))
+    dendrogram(cluster_result,
+               truncate_mode='level',
+               p=100,
+               show_leaf_counts=True,
+               leaf_rotation=90,
+               leaf_font_size=12,
+               show_contracted=True)
+    plt.title('hierarchy tree')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    plt.show()
+
+def show_cluster_samples_detail(vectors, cluster_labels):
+    plt.figure(figsize=(10, 8))
+    plt.title('cluster distribute status')
+    plt.scatter(vectors[:, 0], vectors[:, 1], c=cluster_labels, cmap='prism')
+    plt.show()
+
+
+#根据聚类数目获取
+def get_cluster_labels(cluster_result, cluster_num = 10):
+    return fcluster(cluster_result, t=cluster_num, criterion='maxclust')
+
+
+if __name__ == '__main__':
+    vectors = doc2vec.Doc2Vec.load(doc2vec_model_file)
+    vectors_docs = vectors.docvecs.vectors_docs
+    clusters = train_cluster_by_hierarchical(vectors_docs)
+    show_cluster_progresses(clusters)
+    cluster_labels = get_cluster_labels(clusters)
+    show_cluster_samples_detail(vectors_docs, cluster_labels)
--- src/K_Means_Cluster.py
+++ src/K_Means_Cluster.py
@@ -1,8 +1,10 @@
+# ! -*- coding:utf-8 -*-
 import logging
 
 import os
 from gensim.models import doc2vec
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 from src.Common.FileProcessor import FileProcessor
 
@@ -22,16 +24,24 @@ def save_label_to_tagged_file(cluster_result, tagged_file):
     file_processor.file_write('utf8', u''.join(cluster_samples))
 
 
-def train_cluster(vectors, num_cluster=10):
+def train_cluster_by_kmeans(vectors, num_cluster=10):
     km = KMeans(n_clusters=num_cluster, n_jobs=1)
-    cluster_result = km.fit_predict(vectors.docvecs.vectors_docs)
+    cluster_result = km.fit_predict(vectors)
     return cluster_result
+
+def show_cluster_labels(vectors, cluster_labels):
+    plt.figure(figsize=(10, 8))
+    plt.title('k-means cluser distributes status')
+    plt.scatter(vectors[:, 0], vectors[:, 1], c=cluster_labels, cmap='prism')
+    plt.show()
 
 
 if __name__ == '__main__':
     vectors = doc2vec.Doc2Vec.load(doc2vec_model_file)
-    clusters = train_cluster(vectors)
+    vectors_docs = vectors.docvecs.vectors_docs
+    clusters = train_cluster_by_kmeans(vectors_docs)
     save_label_to_tagged_file(clusters, map_file)
+    show_cluster_labels(vectors_docs, clusters)
 
 
 
